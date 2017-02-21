bac93cafa9fa25c6e673b2dbda1cc2dde61c3e5b
yonic <yonicstudios@gmail.com>
2017-02-21 01:20:33 +0100

    Convert to C++

--- GCAdapter.h
+++ GCAdapter.h
@@ -4,21 +4,25 @@
 #include <libusb-1.0/libusb.h>
 #endif
 #include <sstream>
+#include "ControllerStatus.h"
 
 #pragma once
 
-int AddAdapter(libusb_device *dev);
-NAN_METHOD(Load);
-NAN_METHOD(Setup);
-NAN_METHOD(Request);
-NAN_METHOD(Process);
-NAN_METHOD(Stop);
-NAN_METHOD(RawData);
-void Read();
-bool IsAccessible(libusb_device *dev);
-std::string PollBytes(uint8_t *results);
-unsigned int GetNthBit(uint8_t number, int n);
-v8::Local<v8::Object> GetGamepadStatus(uint8_t * results, int port);
+namespace gca {
 
-const uint16_t GAMECUBE_VID = 0x057E;
-const uint16_t GAMECUBE_PID = 0x0337;
+	int AddAdapter(libusb_device *dev);
+	int Load();
+	int Setup();
+	std::string Request();
+	ControllerStatus* Process();
+	int Stop();
+	std::string RawData();
+	void Read();
+	bool IsAccessible(libusb_device *dev);
+	std::string PollBytes(uint8_t *results);
+	unsigned int GetNthBit(uint8_t number, int n);
+	ControllerStatus GetGamepadStatus(uint8_t * results, int port);
+
+	const uint16_t GAMECUBE_VID = 0x057E;
+	const uint16_t GAMECUBE_PID = 0x0337;
+}
